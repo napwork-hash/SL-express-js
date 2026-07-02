3d6d775e75274ba4e428b7ef9244852a0f6a1235
Napz <prinaf.work@gmail.com>
2026-07-02 11:59:38 +0700

    fix: database port

--- database/index.js
+++ database/index.js
@@ -4,7 +4,7 @@ import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
 const adapter = new PrismaMariaDb({
   host: process.env.DB_HOST || "localhost",
-  port: 3306,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "database",
