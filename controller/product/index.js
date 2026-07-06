19877c2d616e144ce0c9424fee523f6c9652086f
Napz <prinaf.work@gmail.com>
2026-07-06 09:49:45 +0700

    feat: add zod and pagination

--- controller/product/index.js
+++ controller/product/index.js
@@ -3,7 +3,22 @@ import { v4 as uuidv4 } from 'uuid';
 import { asyncHandler } from '../../middleware/errorHandler.js';
 
 const getProducts = asyncHandler(async (req, res) => {
-    const product = await db.product.findMany();
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const maxLimit = 100;
+    const skip = (page - 1) * limit;
+
+    const [product, totalItems] = await Promise.all([
+        db.product.findMany({
+            skip,
+            take: Math.min(limit, maxLimit),
+            orderBy: {
+                name: "asc"
+            }
+        }),
+        db.product.count()
+    ]);
+
     if (product.length <= 0) {
         res.status(200).json({
             status: "success",
@@ -16,7 +31,14 @@ const getProducts = asyncHandler(async (req, res) => {
     res.status(200).json({
         status: "success",
         message: "Berhasil mendapatkan semua data produk",
-        data: product
+        data: product,
+        pagination: {
+            page,
+            limit: limit > maxLimit ? maxLimit : limit,
+            total_items: product.length,
+            total_data: totalItems,
+            total_pages: Math.ceil(totalItems / maxLimit)
+        }
     });
 })
 
@@ -96,6 +118,19 @@ const updateProduct = async (req, res) => {
 
 const deleteProduct = async (req, res) => {
     const { id } = req.params;
+
+    const findId = await db.product.findUnique({
+        where: { id }
+    });
+
+    if (!findId) {
+        console.info("[Produk tidak ditemukan]");
+        return res.status(404).json({
+            status: "error",
+            message: "Produk tidak ditemukan",
+        });
+    }
+
     const deleteProduct = await db.product.delete({
         where: { id }
     })
--- middleware/index.js
+++ middleware/index.js
@@ -1,11 +1,20 @@
-const validateProduct = (req, res, next) => {
-    const { name, price, description } = req.body;
+import * as z from "zod"
 
-    if (!name || !price || !description) {
+const validateProduct = (req, res, next) => {
+    // const { name, price, description } = req.body;
+
+    const productSchema = z.object({
+        name: z.string().min(5, "Nama minimal 5 karakter"),
+        price: z.coerce.number().min(1, "Harga harus diisi"),
+        description: z.string().min(10, "Deskripsi minimal 10 karakter"),
+    });
+
+    const result = productSchema.safeParse(req.body);
+    if (!result.success) {
         console.error("[Data tidak lengkap]");
         return res.status(400).json({
             status: "error",
-            message: "Semua data harus diisi",
+            message: result.error.flatten().fieldErrors
         });
     }
 
@@ -13,9 +22,14 @@ const validateProduct = (req, res, next) => {
 }
 
 const parsePrice = (req, res, next) => {
+
+    if (!req.body.price) {
+        return next();
+    }
+
     const price = Number(req.body.price);
 
-    if (req.body.price === undefined || isNaN(price)){
+    if (req.body.price === undefined || isNaN(price)) {
         console.info("[Price tidak valid]")
         return res.status(400).json({
             status: 'error',
